3fc2d77317d896a619810765a6a3dc269eaa2e0a
MelanieBott2691 <melaniebott@gmail.com>
2020-07-24 16:36:30 -0600

    added togglebutton to nav to create responsive using hamburger bars

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home.js'
-import Contact from './pages/Contact.js'
+import React, { Component } from 'react'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import Home from './pages/Home.js'
+// import Contact from './pages/Contact.js'
 // import Cart from './components/Cart/Cart.js'
 // import Cart from './pages/cart/Cart.js'
-
+import Nav from './components/Nav/Nav'
 // import Cart from './components/Cart/Cart.js'
 
-function App () {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/contact' component={Contact} />
-          {/* <Route exact path='/cart' component={Cart} /> */}
-          {/* <Route exact path="/books/:id" component={Detail} /> */}
-          {/* <Route component={NoMatch} /> */}
-        </Switch>
-      </div>
-    </Router>
-  )
-}
+// function App () {
+//   return (
+//     <Router>
+//       <div>
+//         <Switch>
+//           <Route exact path='/' component={Home} />
+//           <Route exact path='/contact' component={Contact} />
+//           {/* <Route exact path='/cart' component={Cart} /> */}
+//           {/* <Route exact path="/books/:id" component={Detail} /> */}
+//           {/* <Route component={NoMatch} /> */}
+//         </Switch>
+//       </div>
+//     </Router>
+//   )
+// }
 
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Nav />
+        <main style={{ marginTop: '64px' }}>
+          <p>Search Bar Here</p>
+        </main>
+      </div>
+    )
+  }
+}
 export default App
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,37 +1,64 @@
 import React from 'react'
 import './Nav.css'
-import Search from '../Search/Search'
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
 
-export default function Nav () {
-  return (
-    <nav className='navbar' id='navBar'>
-      <ul>
-        <a className='navbar-brand brand' href='/'>
-          Lemonade Stand
-        </a>
-        <a className='navbar-brand contact' href='/contact'>
-          Contact
-        </a>
-        <a className='navbar-brand profile' href='/saved'>
-          Profile
-        </a>
-        <a className='navbar-brand cart' href='/cart'>
-          Cart
-        </a>
-        <a href='javascript:void(0);' class='icon' onclick='myFunction()'>
-          <i class='fa fa-bars'></i>
-        </a>
-      </ul>
-      <Search />
+const navbar = props => (
+  <header className='navbar'>
+    <nav className='navbar__navigation'>
+      <div>
+        <DrawerToggleButton />
+      </div>
+      <div className='navbar__logo'>
+        <a href='/'>Lemonade Stand</a>
+      </div>
+      <div className='spacer'></div>
+      <div className='navbar__navigation-items'>
+        <ul>
+          <li>
+            <a className='navbar-brand contact' href='/contact'>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a className='navbar-brand profile' href='/saved'>
+              Profile
+            </a>
+          </li>
+          <li>
+            <a className='navbar-brand cart' href='/cart'>
+              Cart
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  </header>
+)
+export default navbar
 
-function myFunction () {
-  var x = document.getElementById('navBar')
-  if (x.className === 'navbar') {
-    x.className += ' responsive'
-  } else {
-    x.className = 'navbar'
-  }
-}
+// import Search from '../Search/Search'
+
+// export default function Nav () {
+//   return (
+//     <nav className='navbar' id='navBar'>
+// <ul>
+//   <a className='navbar-brand brand' href='/'>
+//     Lemonade Stand
+//   </a>
+//   <a className='navbar-brand contact' href='/contact'>
+//     Contact
+//   </a>
+//   <a className='navbar-brand profile' href='/saved'>
+//     Profile
+//   </a>
+//   <a className='navbar-brand cart' href='/cart'>
+//     Cart
+//   </a>
+//   <a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+//     <i class='fa fa-bars'></i>
+//   </a>
+// </ul>
+//       <Search />
+//     </nav>
+//   )
+// }
