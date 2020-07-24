2f639318b19cd8aa750bb8f8d6153983f891f611
MelanieBott2691 <melaniebott@gmail.com>
2020-07-24 15:41:25 -0600

    trying to make nav responsive and add bars. NavResponsive needs work

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import Search from '../Search/Search'
 
 export default function Nav () {
   return (
-    <nav className='navbar'>
+    <nav className='navbar' id='navBar'>
       <ul>
         <a className='navbar-brand brand' href='/'>
           Lemonade Stand
@@ -18,8 +18,20 @@ export default function Nav () {
         <a className='navbar-brand cart' href='/cart'>
           Cart
         </a>
+        <a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+          <i class='fa fa-bars'></i>
+        </a>
       </ul>
       <Search />
     </nav>
   )
+}
+
+function myFunction () {
+  var x = document.getElementById('navBar')
+  if (x.className === 'navbar') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navbar'
+  }
 }
