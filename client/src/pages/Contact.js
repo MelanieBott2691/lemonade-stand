8bb8f6ab03e80121703ef36bf2c6ad05b78254d9
MelanieBott2691 <melaniebott@gmail.com>
2020-07-24 00:40:16 -0600

    contact page ready to push to master

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -61,7 +61,7 @@ export default class Contact extends Component {
             </div>
             <div class='form-group'>
               <div class='col-sm-offset-2 col-sm-10'>
-                <button type='submit' class='btn btn-default'>
+                <button type='submit' class='btn'>
                   Submit
                 </button>
               </div>
