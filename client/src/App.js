a60bc23e6d8df0cc977e2e968c852d00b63956cb
MelanieBott2691 <melaniebott@gmail.com>
2020-07-24 12:52:18 -0600

    deleted added folders for signin because the routes didn't work, start over

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Contact from './pages/Contact.js'
 // import Cart from './components/Cart/Cart.js'
-import Cart from './pages/cart/Cart.js'
+// import Cart from './pages/cart/Cart.js'
 
 // import Cart from './components/Cart/Cart.js'
 
@@ -14,7 +14,7 @@ function App () {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/contact' component={Contact} />
-          <Route exact path='/cart' component={Cart} />
+          {/* <Route exact path='/cart' component={Cart} /> */}
           {/* <Route exact path="/books/:id" component={Detail} /> */}
           {/* <Route component={NoMatch} /> */}
         </Switch>
