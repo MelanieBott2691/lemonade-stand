9435d2776df84b3bc145960a268772dcb1efe869
MelanieBott2691 <melaniebott@gmail.com>
2020-07-23 23:00:41 -0600

    added logo to header

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './Nav.css';
-import Search from '../Search/Search';
+import React from 'react'
+import './Nav.css'
+import Search from '../Search/Search'
 
-export default function Nav() {
+export default function Nav () {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className='navbar navbar-dark bg-dark'>
       <ul>
-        <a className="navbar-brand brand" href="/">
-          Google Books
+        <a className='navbar-brand brand' href='/'>
+          Lemonade Stand
         </a>
-        <a className="navbar-brand contact" href="/contact">
+        <a className='navbar-brand contact' href='/contact'>
           Contact
         </a>
-        <a className="navbar-brand saved" href="/saved">
-          Saved
+        <a className='navbar-brand saved' href='/saved'>
+          Profile
         </a>
       </ul>
       <Search />
     </nav>
-  );
+  )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,82 +1,59 @@
 import React, { Component } from 'react'
-import Jumbotron from '../components/Jumbotron'
 import API from '../utils/API'
-import { RecipeList, RecipeListItem } from '../components/RecipeList'
-import { Container, Row, Col } from '../components/Grid'
-import Search from '../components/Search/Search'
+import Nav from '../components/Nav/Nav.js'
+import Results from '../components/Results/Results.js'
+import Footer from '../components/Footer/Footer.js'
 
-class Books extends Component {
+export default class Home extends Component {
   state = {
-    books: [],
-    bookSearch: ''
+    data: [
+      {
+        id: 1,
+        name: 'test1',
+        price: 42.0
+      },
+      {
+        id: 2,
+        name: 'test2',
+        price: 2.0
+      }
+    ],
+    inputSearch: ''
   }
 
-  handleInputChange = event => {
-    const { name, value } = event.target
-    this.setState({
-      [name]: value
-    })
-  }
+  // handleInputChange = (event) => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-  handleFormSubmit = event => {
-    event.preventDefault()
-    API.getBooks(this.state.bookSearch)
-      .then(res => this.setState({ books: res.data }))
-      .catch(err => console.log(err))
-    console.log(this.state.books)
-  }
+  // handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   API.getBooks(this.state.bookSearch)
+  //     .then((res) => this.setState({ books: res.data }))
+  //     .catch((err) => console.log(err));
+  //   console.log(this.state.books);
+  // };
 
-  handleSave = ({ target }) => {
-    const parent = target.parentElement
-    console.log(parent)
-    const title = parent.getElementsByClassName('title')[0].innerHTML
-    const synopsys = parent.getElementsByClassName('synopsys')[0].innerHTML
-    const link = parent.getElementsByClassName('link')[0].innerHTML
-    console.log(title)
-    console.log(synopsys)
-    console.log(link)
-  }
+  // handleSave = ({ target }) => {
+  //   const parent = target.parentElement;
+  //   console.log(parent);
+  //   const title = parent.getElementsByClassName('title')[0].innerHTML;
+  //   const synopsys = parent.getElementsByClassName('synopsys')[0].innerHTML;
+  //   const link = parent.getElementsByClassName('link')[0].innerHTML;
+  //   console.log(title);
+  //   console.log(synopsys);
+  //   console.log(link);
+  // };
 
   render () {
     return (
       <div>
-        <Jumbotron />
-        <Container>
-          <Row>
-            <Col size='md-12'>
-              <Search
-                bookSearch={this.state.bookSearch}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col size='xs-12'>
-              {!this.state.books.length ? (
-                <h1 className='text-center'>Search For An Item or Charity</h1>
-              ) : (
-                <RecipeList>
-                  {this.state.books.map(recipe => {
-                    return (
-                      <RecipeListItem
-                        key={recipe.id}
-                        title={recipe.volumeInfo.title}
-                        href={recipe.volumeInfo.infoLink}
-                        ingredients={recipe.volumeInfo.description}
-                        thumbnail={recipe.volumeInfo.imageLinks.smallThumbnail}
-                        handleSave={this.handleSave}
-                      />
-                    )
-                  })}
-                </RecipeList>
-              )}
-            </Col>
-          </Row>
-        </Container>
+        <Nav />
+        <Results data={this.state.data} />
+        <Footer />
       </div>
     )
   }
 }
-
-export default Books
