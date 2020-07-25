b965d4340acc730e3aa7fcc438ca9d58064ef474
MelanieBott2691 <melaniebott@gmail.com>
2020-07-25 16:07:36 -0600

    added br to seperate carousel slider from navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Contact from './pages/Contact.js'
-import Handler from './components/Handlers/Handler'
 
 function App () {
   return (
@@ -11,8 +10,6 @@ function App () {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/contact' component={Contact} />
-          <Route exact path='/contact' component={Handler} />
-
           {/* <Route exact path="/books/:id" component={Detail} /> */}
           {/* <Route component={NoMatch} /> */}
         </Switch>
