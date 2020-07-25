4b098b05e93d1fd95394e3b72925a23206ba2f68
MelanieBott2691 <melaniebott@gmail.com>
2020-07-25 15:53:14 -0600

    moved hanlder components from app.js to Handler folder, not working, need to figure out path/route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Contact from './pages/Contact.js'
+import Handler from './components/Handlers/Handler'
 
 function App () {
   return (
@@ -10,6 +11,8 @@ function App () {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/contact' component={Handler} />
+
           {/* <Route exact path="/books/:id" component={Detail} /> */}
           {/* <Route component={NoMatch} /> */}
         </Switch>
