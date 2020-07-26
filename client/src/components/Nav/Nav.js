36a06f42acbdf9e77ebc294e6b64c7ddc1099c37
MelanieBott2691 <melaniebott@gmail.com>
2020-07-26 13:40:26 -0600

    navigation works seperate but failed to compile with useEffect and dropdownRef

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,24 +1,27 @@
-import './Nav.css';
+import './Nav.css'
 import React, { useState, useEffect, useRef } from 'react'
-
-// import Contact from './pages/Contact'
-// import EmailIcon from '@material-ui/icons/Email'
-// import BuildIcon from '@material-ui/icons/Build'
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-// import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
-// import SettingsIcon from '@material-ui/icons/Settings'
-// import PersonIcon from '@material-ui/icons/Person'
-// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-// import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-
 import { CSSTransition } from 'react-transition-group'
-// import Search from './components/Search/Search'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Search from './components/Search/Search'
+
+// icons
+import EmailIcon from '@material-ui/icons/Email'
+import BuildIcon from '@material-ui/icons/Build'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import SettingsIcon from '@material-ui/icons/Settings'
+import PersonIcon from '@material-ui/icons/Person'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+
 import { ReactComponent as logo } from './assets/lemonadestandlogo.png'
 
 function Nav () {
   return (
     <Navbar>
-//       <img
+      //{' '}
+      <img
         alt='Lemonade Stand'
         src={logo}
         width='70'
@@ -29,19 +32,20 @@ function Nav () {
       <Search />
       <NavItem icon={<AddShoppingCartIcon />} />
       <NavItem icon={<ExpandMoreIcon />}>
-//         <DropdownMenu></DropdownMenu>
-//       </NavItem>
+        // <DropdownMenu></DropdownMenu>
+        //{' '}
+      </NavItem>
     </Navbar>
-//   )
-// }
+  )
+}
 
-// function Navbar (props) {
-//   return (
-//     <nav className='navbar'>
-//       <ul className='navbar-nav'>{props.children}</ul>
-//     </nav>
-//   )
-// }
+function Navbar (props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}</ul>
+    </nav>
+  )
+}
 
 function NavItem (props) {
   const [open, setOpen] = useState(false)
@@ -54,21 +58,21 @@ function NavItem (props) {
       {open && props.children}
     </li>
   )
- }
+}
 
 function DropdownMenu () {
   const [activeMenu, setActiveMenu] = useState('main')
   const [menuHeight, setMenuHeight] = useState(null)
   const dropdownRef = useRef(null)
 
-//   useEffect(() => {
-//     setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
-//   }, [])
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
 
-//   function calcHeight (el) {
-//     const height = el.offsetHeight
-//     setMenuHeight(height)
-//   }
+  function calcHeight (el) {
+    const height = el.offsetHeight
+    setMenuHeight(height)
+  }
 
   function DropdownItem (props) {
     return (
@@ -77,10 +81,12 @@ function DropdownMenu () {
         className='menu-item'
         onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
       >
-       <span className='icon-button'>{props.leftIcon}</span>{props.children}
-  <span className='icon-right'>{props.rightIcon}</span></a>
-)
-}
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+        <span className='icon-right'>{props.rightIcon}</span>
+      </a>
+    )
+  }
 
   return (
     <div className='dropdown' style={{ height: menuHeight }} ref={dropdownRef}>
@@ -91,58 +97,58 @@ function DropdownMenu () {
         unmountOnExit
         onEnter={calcHeight}
       >
-//         <div className='menu'>
-//           <DropdownItem leftIcon={<PersonIcon />} goToMenu='profile'>
-//             My Profile
-//           </DropdownItem>
-//           <DropdownItem leftIcon={<SettingsIcon />} goToMenu='settings'>
-//             Settings
-//           </DropdownItem>
-//           <DropdownItem leftIcon={<EmailIcon />} goToMenu={Contact}>
-//             Contact
-//           </DropdownItem>
-//         </div>
-//       </CSSTransition>
+        <div className='menu'>
+          <DropdownItem leftIcon={<PersonIcon />} goToMenu='profile'>
+            My Profile
+          </DropdownItem>
+          <DropdownItem leftIcon={<SettingsIcon />} goToMenu='settings'>
+            Settings
+          </DropdownItem>
+          <DropdownItem leftIcon={<EmailIcon />} goToMenu={Contact}>
+            Contact
+          </DropdownItem>
+        </div>
+      </CSSTransition>
 
-//       <CSSTransition
-//         in={activeMenu === 'settings'}
-//         timeout={500}
-//         classNames='menu-secondary'
-//         unmountOnExit
-//         onEnter={calcHeight}
-//       >
-//         <div className='menu'>
-//           <DropdownItem goToMenu='main' leftIcon={<ChevronLeftIcon />}>
-//             <h2>Main Menu</h2>
-//           </DropdownItem>
-//           <DropdownItem leftIcon={<BuildIcon />}>HTML</DropdownItem>
-//           <DropdownItem leftIcon={<BuildIcon />}>CSS</DropdownItem>
-//           <DropdownItem leftIcon={<BuildIcon />}>JavaScript</DropdownItem>;
-//           <DropdownItem leftIcon={<EmailIcon />} goToMenu={Contact}>
-//             Contact
-//           </DropdownItem>
-//         </div>
-//       </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<ChevronLeftIcon />}>
+            <h2>Main Menu</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BuildIcon />}>Menu</DropdownItem>
+          <DropdownItem leftIcon={<BuildIcon />}>Menu</DropdownItem>
+          <DropdownItem leftIcon={<BuildIcon />}>Menu</DropdownItem>;
+          <DropdownItem leftIcon={<EmailIcon />} goToMenu={Home}>
+            Home
+          </DropdownItem>
+        </div>
+      </CSSTransition>
 
-//       <CSSTransition
-//         in={activeMenu === 'animals'}
-//         timeout={500}
-//         classNames='menu-secondary'
-//         unmountOnExit
-//         onEnter={calcHeight}
-//       >
-//         <div className='menu'>
-//           <DropdownItem goToMenu='main' leftIcon={<KeyboardArrowDownIcon />}>
-//             <h2>Animals</h2>
-//           </DropdownItem>
-//           <DropdownItem leftIcon='🦘'>Kangaroo</DropdownItem>
-//           <DropdownItem leftIcon='🐸'>Frog</DropdownItem>
-//           <DropdownItem leftIcon='🦋'>Horse?</DropdownItem>
-//           <DropdownItem leftIcon='🦔'>Hedgehog</DropdownItem>
-//         </div>
-//       </CSSTransition>
-//     </div>
-//   )
-// }
+      <CSSTransition
+        in={activeMenu === 'login'}
+        timeout={500}
+        classNames='menu-third'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<KeyboardArrowDownIcon />}>
+            <h2>Login</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon='🦘'>Kangaroo</DropdownItem>
+          <DropdownItem leftIcon='🐸'>Frog</DropdownItem>
+          <DropdownItem leftIcon='🦋'>Horse?</DropdownItem>
+          <DropdownItem leftIcon='🦔'>Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  )
+}
 
-// export default Nav
+export default Nav
