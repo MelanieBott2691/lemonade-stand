6b47f827f75ed7a453dc9e3e3a5527a54011fed9
MelanieBott2691 <melaniebott@gmail.com>
2020-07-26 12:09:56 -0600

    used React.StrictMode in index.js to wrap the Nav and App. StrictMode helps identify components with unsafe lifecycles, side effects, and current rendering

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Nav from './components/Nav/Nav'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <Nav />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
