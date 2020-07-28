e3dee30e6de326b053b439f849d8e7e74572567d
MelanieBott2691 <melaniebott@gmail.com>
2020-07-28 02:51:29 -0600

    Home.js set up to export data for items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // import API from '../utils/API'
-import Navbar from '../components/Navbar/Navbar.js'
+// import Navbar from '../components/Navbar/Navbar.js'
 import Results from '../components/Results/Results.js'
 import Carouselbanner from '../components/Carousel/Carousel.js'
 // import Footer from '../components/Footer/Footer.js';
@@ -37,39 +37,12 @@ export default class Home extends Component {
     inputSearch: ''
   }
 
-  // handleInputChange = (event) => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
-
-  // handleFormSubmit = (event) => {
-  //   event.preventDefault();
-  //   API.getBooks(this.state.bookSearch)
-  //     .then((res) => this.setState({ books: res.data }))
-  //     .catch((err) => console.log(err));
-  //   console.log(this.state.books);
-  // };
-
-  // handleSave = ({ target }) => {
-  //   const parent = target.parentElement;
-  //   console.log(parent);
-  //   const title = parent.getElementsByClassName('title')[0].innerHTML;
-  //   const synopsys = parent.getElementsByClassName('synopsys')[0].innerHTML;
-  //   const link = parent.getElementsByClassName('link')[0].innerHTML;
-  //   console.log(title);
-  //   console.log(synopsys);
-  //   console.log(link);
-  // };
-
   render () {
     return (
       <div>
-        <Navbar />
+        {/* <Nav /> */}
         <br></br>
         <Carouselbanner />
-        <br></br>
         <Results data={this.state.data} />
         {/* <Footer /> */}
       </div>
