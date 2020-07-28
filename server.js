03eece61c0416ede4501da3dd0faa5f54c0dd1bd
MelanieBott2691 <melaniebott@gmail.com>
2020-07-28 03:12:03 -0600

    error invalid hook call

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const path = require('path');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-const routes = require('./routes');
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// // const apiRoutes = require('./routes/apiRoutes');
+// const routes = require('./routes');
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
 
-// Use apiRoutes
-// app.use('/api', apiRoutes);
-app.use(routes);
+// // Use apiRoutes
+// // app.use('/api', apiRoutes);
+// app.use(routes);
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/lemonadestand',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/lemonadestand',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }
+// );
 
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// app.listen(PORT, function () {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
