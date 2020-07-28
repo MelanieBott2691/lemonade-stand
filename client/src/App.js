44bf4629391f1b7deb7f71fc27febf1ae960cc71
MelanieBott2691 <melaniebott@gmail.com>
2020-07-28 02:38:11 -0600

    import store using Provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Contact from './pages/Contact.js'
+import { Provider } from 'react-redux'
 
 function App () {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/contact' component={Contact} />
-          {/* <Route exact path="/books/:id" component={Detail} /> */}
-          {/* <Route component={NoMatch} /> */}
-        </Switch>
-      </div>
+      <Provider>
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/contact' component={Contact} />
+            {/* <Route exact path="/books/:id" component={Detail} /> */}
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   )
 }
