8989f0f82e10a8d6cd0de89b494d461ec81e67cc
MelanieBott2691 <melaniebott@gmail.com>
2020-07-29 14:47:30 -0600

    Parsing error in nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,33 +3,41 @@ import './Nav.css'
 
 export default function Nav () {
   return (
-    <div className='btn-group'>
-      <button type='button' className='btn btn-danger'>
-        Action
-      </button>
-      <button
-        type='button'
-        className='btn dropdown-toggle dropdown-toggle-split'
-        data-toggle='dropdown'
-        aria-haspopup='true'
-        aria-expanded='false'
+    <div className='nav-header'>
+      <header
+        role='banner'
+        className='NavMain-Banner navbar-expand navbar-dark'
       >
-        <span className='sr-only'>Toggle Dropdown</span>
-      </button>
-      <div className='dropdown-menu'>
-        <a className='dropdown-item' href='/Home'>
+        <a href='/'></a>
+      </header>
+      <div className='btn-group'>
+        <button type='button' className='btn btn-danger'>
           Action
-        </a>
-        <a className='dropdown-item' href='/Login'>
-          Another action
-        </a>
-        <a className='dropdown-item' href='/Contact'>
-          Something else here
-        </a>
-        <div class='dropdown-divider'></div>
-        <a className='dropdown-item' href='/Cart'>
-          Separated link
-        </a>
+        </button>
+        <button
+          type='button'
+          className='btn dropdown-toggle dropdown-toggle-split'
+          data-toggle='dropdown'
+          aria-haspopup='true'
+          aria-expanded='false'
+        >
+          <span className='sr-only'>Toggle Dropdown</span>
+        </button>
+        <div className='dropdown-menu'>
+          <a className='dropdown-item' href='/Home'>
+            Action
+          </a>
+          <a className='dropdown-item' href='/Login'>
+            Another action
+          </a>
+          <a className='dropdown-item' href='/Contact'>
+            Something else here
+          </a>
+          <div class='dropdown-divider'></div>
+          <a className='dropdown-item' href='/Cart'>
+            Separated link
+          </a>
+        </div>
       </div>
     </div>
   )
