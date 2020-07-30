9225c7d963b97c5c4f0c83408da0cfbbc69812f2
MelanieBott2691 <melaniebott@gmail.com>
2020-07-29 23:18:47 -0600

    modal to hold checkout form

---
+++ client/src/components/Checkout/CheckoutSteps.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Form, Modal, Button } from 'react-bootstrap'
+
+function CheckoutSteps (props) {
+  return (
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Checkout</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId='exampleForm.ControlInput1'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='name@example.com' />
+          </Form.Group>
+          <Form.Group controlId='exampleForm.ControlSelect1'>
+            <Form.Label>Example select</Form.Label>
+            <Form.Control as='select'>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='exampleForm.ControlSelect2'>
+            <Form.Label>Example multiple select</Form.Label>
+            <Form.Control as='select' multiple>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='exampleForm.ControlTextarea1'>
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as='textarea' rows='3' />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary'>Close</Button>
+        <Button variant='primayr'>Save Changes</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  )
+}
+
+export default CheckoutSteps
