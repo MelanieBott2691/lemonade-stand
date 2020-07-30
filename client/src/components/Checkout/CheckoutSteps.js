2a97c0f27f72e43fa6ccacd7f953f1270fe42951
MelanieBott2691 <melaniebott@gmail.com>
2020-07-30 17:40:59 -0600

    added checkout modal to cart

--- client/src/components/Checkout/CheckoutSteps.js
+++ client/src/components/Checkout/CheckoutSteps.js
@@ -1,56 +1,25 @@
-import { Modal } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 
-import React, { Component } from 'react'
-import PayModal from '../Checkout/Modal'
+import React from 'react'
+// import PayModal from '../Checkout/Modal'
 // import TriggerButton from '../TriggerButton'
 
-export class CheckoutSteps extends Component {
-  state = { isShown: false }
-  showModal = () => {
-    this.setState({ isShown: true }, () => {
-      this.closeButton.focus()
-    })
-    this.toggleScrollLock()
-  }
-  closeModal = () => {
-    this.setState({ isShown: false })
-    this.focus()
-    this.toggleScrollLock()
-  }
-  onKeyDown = event => {
-    if (event.keyCode === 27) {
-      this.closeModal()
-    }
-  }
-  onClickOutside = event => {
-    if (this.modal && this.modal.contains(event.target)) return
-    this.closeModal()
-  }
-  toggleScrollLock = () => {
-    document.querySelector('html').classList.toggle('scroll-lock')
-  }
-  render () {
-    return (
-      <React.Fragment>
-        {/* <TriggerButton */}
-        <button onClick={PayModal}>
-          Pay
-          {/* showModal={this.showModal}
-          buttonRef={n => (this.TriggerButton = n)}
-          triggerText={this.props.triggerText} */}
-        </button>
-        {this.state.isShown ? (
-          <Modal
-            onSubmit={this.props.onSubmit}
-            modalRef={n => (this.modal = n)}
-            buttonRef={n => (this.closeButton = n)}
-            closeModal={this.closeModal}
-            onKeyDown={this.onKeyDown}
-            onClickOutside={this.onClickOutside}
-          />
-        ) : null}
-      </React.Fragment>
-    )
-  }
+function CheckoutSteps () {
+  return (
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Checkout</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>Checkout Form HERE....</p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant='secondary'>Cancel</Button>
+        <Button variant='primary'>Pay</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  )
 }
 export default CheckoutSteps
