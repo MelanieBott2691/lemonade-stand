190addead1d2799134c495fb390f66c5298d1b3b
Kell Banks <mckellbanks@gmail.com>
2020-07-31 14:38:10 -0600

    cleaned up code and created some bootstrap formatting for the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,9 @@ import Nav from '../components/Nav/Nav';
 import UserInfo from '../components/UserInfo/UserInfo';
 import StoreInfo from '../components/StoreInfo/StoreInfo';
 import PurchaseInfo from '../components/PurchaseInfo/PurchaseInfo';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class Profile extends Component {
   state = {
@@ -19,9 +22,24 @@ export default class Profile extends Component {
     return (
       <>
         <Nav />
-        <UserInfo user={this.state.user} />
-        <StoreInfo />
-        <PurchaseInfo />
+        <Container>
+          <Row> 
+            <Col>         
+              <UserInfo user={this.state.user} />
+            </Col> 
+          </Row>  
+          <Row>
+            <Col>
+              <StoreInfo />
+            </Col>
+          </Row>  
+          <Row>
+            <Col>
+              <PurchaseInfo />
+            </Col>
+          </Row>  
+          
+        </Container>
       </>
     );
   }
