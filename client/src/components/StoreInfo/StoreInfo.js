b86d8f17a2d6605529f7cc408d0680bc45fbf6fe
Kell Banks <mckellbanks@gmail.com>
2020-07-30 23:58:43 -0600

    created a basic my store comp placeholder. Needs styling and will require mapping similar to the results wrapper

--- client/src/components/StoreInfo/StoreInfo.js
+++ client/src/components/StoreInfo/StoreInfo.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import './StoreInfo.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import logo from '../StoreInfo/logo.png';
 
 export default function StoreInfo() {
-  return <div>Store stuff</div>;
+  return (
+    <Card>
+      <Card.Img className="logo"
+        variant="top"
+        src={logo}
+      />
+      <Card.Body>
+        <Card.Title>My Stands</Card.Title>
+        <Button href="#">
+          Create New Stand
+        </Button> <Button href="#">Edit Stand</Button>{' '}
+      </Card.Body>
+    </Card>
+  );
 }
+
