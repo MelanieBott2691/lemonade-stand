d409a772d6498b4f52b911d6e187b4530de1be67
MelanieBott2691 <melaniebott@gmail.com>
2020-08-01 21:45:44 -0600

    added About page with css in style.css

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Profile from './pages/Profile.js';
 import CreateStore from './pages/CreateStore.js';
 import StoreItem from './components/Store/StoreItem.js';
 import Store from './pages/Store';
+import About from './pages/About';
 import Footer from './components/Footer/Footer.js';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
