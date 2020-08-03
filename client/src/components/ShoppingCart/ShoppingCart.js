7634986b9b7ecf156cf112a22fb0f403038d6141
MelanieBott2691 <melaniebott@gmail.com>
2020-08-02 23:08:33 -0600

    created product, price, quantity, total for shopping cart

--- client/src/components/ShoppingCart/ShoppingCart.js
+++ client/src/components/ShoppingCart/ShoppingCart.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
 
 import CheckoutModal from '../Checkout/CheckoutModal';
-import { Form, Row, Col, Card } from 'react-bootstrap';
+import { Form, Row, Col, Card, Table } from 'react-bootstrap';
 
 export default class ShoppingCart extends Component {
   render() {
@@ -10,7 +10,130 @@ export default class ShoppingCart extends Component {
       <>
         <Nav />
         <Card>
-          <Form>
+          <Card.Header>My Shopping Cart</Card.Header>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Qauntity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>
+                  {
+                    <Form.Control
+                      as="select"
+                      className="mr-sm-2"
+                      id="inlineFormCustomSelect"
+                      custom>
+                      <option value="0">Choose Qauntity...</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="other">Other (add in notes)</option>
+                    </Form.Control>
+                  }
+                </td>
+                <td>{}</td>
+              </tr>
+              <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>
+                  {
+                    <Form.Control
+                      as="select"
+                      className="mr-sm-2"
+                      id="inlineFormCustomSelect"
+                      custom>
+                      <option value="0">Choose Qauntity...</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="other">Other (add in notes)</option>
+                    </Form.Control>
+                  }
+                </td>
+                <td>{}</td>
+              </tr>
+              <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>
+                  {
+                    <Form.Control
+                      as="select"
+                      className="mr-sm-2"
+                      id="inlineFormCustomSelect"
+                      custom>
+                      <option value="0">Choose Qauntity...</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="other">Other (add in notes)</option>
+                    </Form.Control>
+                  }
+                </td>
+                <td>{}</td>
+              </tr>
+              <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>
+                  {
+                    <Form.Control
+                      as="select"
+                      className="mr-sm-2"
+                      id="inlineFormCustomSelect"
+                      custom>
+                      <option value="0">Choose Qauntity...</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="other">Other (add in notes)</option>
+                    </Form.Control>
+                  }
+                </td>
+                <td>{}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card>
+        {/* <Card> */}
+        {/* <Form>
             <Form.Row className="align-items-center">
               <Col xs="auto" className="my-1">
                 <Form.Control
@@ -34,7 +157,7 @@ export default class ShoppingCart extends Component {
               </Col>
             </Form.Row>
           </Form>
-        </Card>
+        </Card> */}
         <Card>
           <Form>
             <Row>
