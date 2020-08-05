7f18c2b767b1f4c45547aa4976ee48c640144ae0
Kell Banks <mckellbanks@gmail.com>
2020-08-04 21:41:29 -0600

    created the edit item modal and layout

--- client/src/components/NewStore/NewStore.js
+++ client/src/components/NewStore/NewStore.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import './NewStore.css';
 
-import {
-  Modal,
-  Button,
-  Card,
-  Row,
-  Col,
-  Form,
-  Container,
-} from 'react-bootstrap';
+import { Modal, Button, Col, Form, Container } from 'react-bootstrap';
 
 class NewStore extends React.Component {
   constructor() {
@@ -58,26 +50,6 @@ class NewStore extends React.Component {
                         label="Upload an Image for your Stand"
                       />
                     </Form.Group>
-                    {/* <Form.Group controlId="exampleForm.ControlSelect1">
-                      <Form.Label>Example select</Form.Label>
-                      <Form.Control as="select">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect2">
-                      <Form.Label>Example multiple select</Form.Label>
-                      <Form.Control as="select" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </Form.Control>
-                    </Form.Group> */}
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                       <Form.Label>Stand Description</Form.Label>
                       <Form.Control
