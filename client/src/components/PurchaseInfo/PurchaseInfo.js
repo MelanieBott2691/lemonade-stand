0fb51e5c069603706ae83a97049ac820c269a362
Kell Banks <mckellbanks@gmail.com>
2020-08-05 23:25:46 -0600

    created static data for the recent purchases comp on the profile page

--- client/src/components/PurchaseInfo/PurchaseInfo.js
+++ client/src/components/PurchaseInfo/PurchaseInfo.js
@@ -8,20 +8,61 @@ export default function PurchaseInfo(props) {
     <Card>
       <Card.Body>
         <Card.Title>Recent Purchases</Card.Title>
-        <ListGroup>
-          <ListGroup.Item>
-            <Card.Img variant="top" src="holder.js/100px180" /> Price: $50
-            Status: Shipped{' '}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Card.Img variant="top" src="holder.js/100px180" /> Price: $40
-            Status: Delivered{' '}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Card.Img variant="top" src="holder.js/100px180" /> Price: $30
-            Status: Order Processing
-          </ListGroup.Item>
-        </ListGroup>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Product</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>
+                  <img
+                    className="img-size"
+                    variant="top"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQI5bVzb97IoZ53rt_CqeXv_ifuyizhMSolWQ&usqp=CAU"
+                    thumbnail
+                  />
+                </td>
+                <td>Friendship Bracelets</td>
+                <td>$5.00</td>
+                <td>Delivered</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>
+                  <img
+                    className="img-size"
+                    variant="top"
+                    src="https://cdn.shopify.com/s/files/1/1133/3328/products/yummy-luv-sticker-pack-2019_800x.png?v=1564258685"
+                  />
+                </td>
+                <td>Cute Sticker Pack</td>
+                <td>$10.00</td>
+                <td>Shipped</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>
+                  <img
+                    className="img-size"
+                    variant="top"
+                    src="https://images-na.ssl-images-amazon.com/images/I/61LKtry2iOL._AC_SX425_.jpg"
+                  />
+                </td>
+                <td>Fluffy Slime</td>
+                <td>$6.00</td>
+                <td>Order Processing</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </Card.Body>
     </Card>
   );
