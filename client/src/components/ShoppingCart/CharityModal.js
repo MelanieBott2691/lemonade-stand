24e7f4e1deb07216a143e93102e1f65b3fc0db6b
MelanieBott2691 <melaniebott@gmail.com>
2020-08-06 15:40:19 -0600

    changed react to html and able to link websites to charity options

--- client/src/components/ShoppingCart/CharityModal.js
+++ client/src/components/ShoppingCart/CharityModal.js
@@ -63,119 +63,57 @@ class CharityModal extends React.Component {
               </Col>
             </Container>
           </Modal.Header>
-
           <Modal.Body className="modal-container">
             <Card.Body>
               <Card.Title>Choose a Charity To Donate To:</Card.Title>
               <br />
-              <Row>
-                {/* test */}
-                <Col>
-                  <a href="https//www.google.com">Charity</a>
-                </Col>
 
-                {/* test end */}
-                <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          className="charity-label"
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Red Cross`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col>
-                <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          className="charity-label"
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Local Charity`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col>
-                <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          className="charity-label"
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Make A Wish`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          className="charity-label"
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Habitat for Humanity`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col>
-                {/* <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Local Charity`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col> */}
-                <Col>
-                  <Form>
-                    {['radio'].map((type) => (
-                      <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                          type={type}
-                          id={`default-${type}`}
-                          label={`Best Friends Animal Society`}
-                        />
-                      </div>
-                    ))}
-                  </Form>
-                </Col>
-              </Row>
+              <div className="row">
+                <div className="column">
+                  <a
+                    href="https://secure2.wish.org/site/SPageServer?pagename=donate_now&ft=SPEA&fi=20_10&otgmthg=true&level1=1000&level2=500&level3=250&level4=125&level5=75&presel=level4&Campaign_ID=MNOO%25fy%25&Appeal_ID=%25fy%25ON-XXX-SRCH&Package_ID=OT-NT-GIO&Subtype=DEV-Multimedia%20Online&gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_term=make-a-wish%20donate&utm_campaign=Paid&gclid=CjwKCAjw1K75BRAEEiwAd41h1P9D5CR7ax2-0HQOND931UGC7V6Qs_1dmJ-dZbZCsPEwsAqAXl9NURoCZxEQAvD_BwE"
+                    target="_blank">
+                    Make A Wish
+                  </a>
+                </div>
+              </div>
+
+              <div className="column">
+                <a
+                  href="https://www.redcross.org/donate/D072002.html/?adobe_mc_sdid=SDID%3D05FDF77404FC8745-39ECDBA5D918592D%7CMCORGID%3D723A22C757518E2C7F000101@AdobeOrg%7CTS%3D1596749763&adobe_mc_ref=https:%2F%2Fwww.google.com%2F"
+                  target="_blank">
+                  Red Cross
+                </a>
+              </div>
+
+              <div className="row">
+                <div className="column">
+                  <a
+                    href="https://www.habitat.org/together-we-build?source_code=DNWW14071000043&mrkgadid=3311692909&mrkgcl=1005&mrkgen=gtext&mrkgbflag=1&mrkgcat=exact&device=c&network=g&matchtype=e&locationid=%7bloc_phyiscal_ms%7d&creative=318577458933&targetid=kwd-15438344272&campaignid=138111921&adgroupid=8353760961&gclid=CjwKCAjw1K75BRAEEiwAd41h1KU9pDapgo7jbPPnjI-E2XxmWUKLfQeitqZsvHm25Ce0ldsscnm0eBoCjX4QAvD_BwE&gclsrc=aw.ds"
+                    target="_blank">
+                    Habitat for Humanity
+                  </a>
+                </div>
+              </div>
+
+              <div className="column">
+                <a
+                  href="https://support.bestfriends.org/site/Donation2?df_id=2884&mfc_pref=T&2884.donation=form1&gclid=CjwKCAjw1K75BRAEEiwAd41h1DugvBFWpL1rTykJeiOx-qVmWsMH0kWO8SygYEz0rH41VhfmMg-MrBoC5Q0QAvD_BwE"
+                  target="_blank">
+                  Best Friends Animal Society
+                </a>
+              </div>
+              <div className="column">
+                <a href="/shoppingcart">Local Charity</a>
+              </div>
+              <Button
+                className="checkout-btn"
+                variant="primary"
+                onClick={() => this.handleModalShowHide()}>
+                Return to Complete Checkout
+              </Button>
             </Card.Body>
           </Modal.Body>
-          <Modal.Footer>
-            {/* <Button
-              className="cancel-btn"
-              variant="secondary"
-              onClick={() => this.handleModalShowHide()}>
-              Cancel
-            </Button> */}
-            <Button
-              className="checkout-btn"
-              variant="primary"
-              onClick={() => this.handleModalShowHide()}>
-              Return to Complete Checkout
-            </Button>
-          </Modal.Footer>
         </Modal>
       </div>
     );
