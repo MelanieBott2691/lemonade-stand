9e68e9d576bc2d4c51ec808c5cf3984e131406b2
MelanieBott2691 <melaniebott@gmail.com>
2020-08-06 15:54:51 -0600

    select charity modal completed with links to donation source

--- client/src/components/ShoppingCart/CharityModal.js
+++ client/src/components/ShoppingCart/CharityModal.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import RadioButton from '../Checkout/RadioButton';
-import {
-  Modal,
-  Button,
-  Form,
-  Card,
-  Row,
-  Col,
-  Container
-} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import RadioButton from '../Checkout/RadioButton';
+import { Modal, Button, Card, Row, Col, Container } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
 import charity from '../../assets/images/charity.png';
 import './CharityModal.css';
 
@@ -41,35 +33,25 @@ class CharityModal extends React.Component {
         <Modal id="modal" show={this.state.showHide} className="modal-checkout">
           <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
             <Container>
-              <Col>
-                <Row>
+              <Row>
+                <Col>
                   <img
                     src={charity}
-                    alt="header image"
-                    variant="top"
+                    alt="charity"
                     height={200}
                     width={400}
                     className="card-image"
                   />
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <hr />
-                  <Modal.Title></Modal.Title>
-                </Row>
-              </Col>
+                </Col>
+              </Row>
             </Container>
           </Modal.Header>
           <Modal.Body className="modal-container">
             <Card.Body>
               <Card.Title>Choose a Charity To Donate To:</Card.Title>
-              <br />
 
-              <div className="row">
-                <div className="column">
+              <div className="row-charity">
+                <div className="column-charity">
                   <a
                     href="https://secure2.wish.org/site/SPageServer?pagename=donate_now&ft=SPEA&fi=20_10&otgmthg=true&level1=1000&level2=500&level3=250&level4=125&level5=75&presel=level4&Campaign_ID=MNOO%25fy%25&Appeal_ID=%25fy%25ON-XXX-SRCH&Package_ID=OT-NT-GIO&Subtype=DEV-Multimedia%20Online&gclsrc=aw.ds&&utm_source=google&utm_medium=cpc&utm_term=make-a-wish%20donate&utm_campaign=Paid&gclid=CjwKCAjw1K75BRAEEiwAd41h1P9D5CR7ax2-0HQOND931UGC7V6Qs_1dmJ-dZbZCsPEwsAqAXl9NURoCZxEQAvD_BwE"
                     target="_blank">
@@ -78,7 +60,7 @@ class CharityModal extends React.Component {
                 </div>
               </div>
 
-              <div className="column">
+              <div className="column-charity">
                 <a
                   href="https://www.redcross.org/donate/D072002.html/?adobe_mc_sdid=SDID%3D05FDF77404FC8745-39ECDBA5D918592D%7CMCORGID%3D723A22C757518E2C7F000101@AdobeOrg%7CTS%3D1596749763&adobe_mc_ref=https:%2F%2Fwww.google.com%2F"
                   target="_blank">
@@ -86,8 +68,8 @@ class CharityModal extends React.Component {
                 </a>
               </div>
 
-              <div className="row">
-                <div className="column">
+              <div className="row-charity">
+                <div className="column-charity">
                   <a
                     href="https://www.habitat.org/together-we-build?source_code=DNWW14071000043&mrkgadid=3311692909&mrkgcl=1005&mrkgen=gtext&mrkgbflag=1&mrkgcat=exact&device=c&network=g&matchtype=e&locationid=%7bloc_phyiscal_ms%7d&creative=318577458933&targetid=kwd-15438344272&campaignid=138111921&adgroupid=8353760961&gclid=CjwKCAjw1K75BRAEEiwAd41h1KU9pDapgo7jbPPnjI-E2XxmWUKLfQeitqZsvHm25Ce0ldsscnm0eBoCjX4QAvD_BwE&gclsrc=aw.ds"
                     target="_blank">
@@ -96,16 +78,17 @@ class CharityModal extends React.Component {
                 </div>
               </div>
 
-              <div className="column">
+              <div className="column-charity">
                 <a
                   href="https://support.bestfriends.org/site/Donation2?df_id=2884&mfc_pref=T&2884.donation=form1&gclid=CjwKCAjw1K75BRAEEiwAd41h1DugvBFWpL1rTykJeiOx-qVmWsMH0kWO8SygYEz0rH41VhfmMg-MrBoC5Q0QAvD_BwE"
                   target="_blank">
                   Best Friends Animal Society
                 </a>
               </div>
-              <div className="column">
+              <div className="column-charity">
                 <a href="/shoppingcart">Local Charity</a>
               </div>
+              <hr />
               <Button
                 className="checkout-btn"
                 variant="primary"
